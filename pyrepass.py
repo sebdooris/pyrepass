973f92bad68e7258ae376c4c0f382aa19f0d9426
Seb Dooris <sebastian.dooris@gmail.com>
2018-02-26 15:52:47 +0000

    Initial commit

---
+++ pyrepass.py
@@ -0,0 +1,35 @@
+import pandas as pd
+import re
+def password_check(password):
+    length = len(password)
+    lower = 0
+    upper = 0
+    numeric = 0
+    special = 0
+    space = 0
+    for c in password:
+        if re.search(r"[a-z]", c):
+            lower += 1
+        if re.search(r"[A-Z]", c):
+            upper += 1
+        if re.search(r"[0-9]", c):
+            numeric += 1
+        if re.search(r"[ !#$%&'()*+,-./[\\\]^_`{|}~"+r'"]', c):
+            special += 1
+        if re.search(r' ', c):
+            space += 1
+
+    return {
+        'length': length,
+        'lower': lower,
+        'upper': upper,
+        'numeric': numeric,
+        'special': special,
+        'password': password,
+        'space': space,
+    }
+# Read csv
+data = pd.read_csv("accounts.csv")
+for password in data['Password']:
+    result = password_check(password)
+    print(result)
